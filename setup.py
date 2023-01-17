0a0294d24d1cbf25aa4200801ef5fbe5f98d8160
DPG <Ricky20061204@163.com>
2023-01-17 09:23:33 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'hcgamelib',
+  packages = ['hcgamelib'],
+  version = '0.1',
+  license='WTFPL',
+  description = 'Games!',
+  author = 'Doppelganger',
+  author_email = 'admin@doppelganger.eu.org',
+  url = 'https://github.com/Doppelganger-phi',
+  download_url = 'https://github.com/Doppelganger-phi/hcgamelib/archive/v_01.tar.gz',
+  keywords = ['games','hc'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: WTFPL',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+  ],
+)
